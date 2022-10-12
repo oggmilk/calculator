0959bc838770ca484ab312048277119e4c84d7fc
oggmilk <mattdragicevic@gmail.com>
2022-10-12 13:18:28 -0700

    Pushes buttonVal pressed to calc screens currVal

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ const six = document.querySelector("#six");
 const seven = document.querySelector("#seven");
 const eight = document.querySelector("#eight");
 const nine = document.querySelector("#nine");
+
+
+zero.addEventListener("click", () => {
+    currentVal.textContent += 0;
+});
+
+one.addEventListener("click", () => {
+    currentVal.textContent += 1;
+});
+
+two.addEventListener("click", () => {
+    currentVal.textContent += 2;
+});
+
+three.addEventListener("click", () => {
+    currentVal.textContent += 3;
+});
+
+four.addEventListener("click", () => {
+    currentVal.textContent += 4;
+});
+
+five.addEventListener("click", () => {
+    currentVal.textContent += 5;
+});
+
+six.addEventListener("click", () => {
+    currentVal.textContent += 6;
+});
+
+seven.addEventListener("click", () => {
+    currentVal.textContent += 7;
+});
+
+eight.addEventListener("click", () => {
+    currentVal.textContent += 8;
+});
+
+nine.addEventListener("click", () => {
+    currentVal.textContent += 9;
+});
