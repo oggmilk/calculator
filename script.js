7ba05e1ac2d0307cb86a58f003408a12fa3d232d
oggmilk <mattdragicevic@gmail.com>
2022-10-12 13:15:05 -0700

    Creates variables linked to html ID selectors

---
+++ script.js
@@ -0,0 +1,12 @@
+const currentVal = document.querySelector("#current-value");
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
