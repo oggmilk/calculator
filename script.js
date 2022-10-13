b76cd3728ac43a847da21c9bf93c9947f43d4525
oggmilk <mattdragicevic@gmail.com>
2022-10-13 13:08:31 -0700

    Add math functionality

--- script.js
+++ script.js
@@ -15,39 +15,50 @@ const nine = document.querySelector("#nine");
 zero.addEventListener("click", () => {
     currentVal.textContent += 0;
 });
-
 one.addEventListener("click", () => {
     currentVal.textContent += 1;
 });
-
 two.addEventListener("click", () => {
     currentVal.textContent += 2;
 });
-
 three.addEventListener("click", () => {
     currentVal.textContent += 3;
 });
-
 four.addEventListener("click", () => {
     currentVal.textContent += 4;
 });
-
 five.addEventListener("click", () => {
     currentVal.textContent += 5;
 });
-
 six.addEventListener("click", () => {
     currentVal.textContent += 6;
 });
-
 seven.addEventListener("click", () => {
     currentVal.textContent += 7;
 });
-
 eight.addEventListener("click", () => {
     currentVal.textContent += 8;
 });
-
 nine.addEventListener("click", () => {
     currentVal.textContent += 9;
 });
+
+function operate(num1, num2, operator){
+    return operator(num1, num2);
+}
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
